970d8db0a386bb9100044b47409974161e52f829
GigaShavliashvili <87610428+GigaShavliashvili@users.noreply.github.com>
2022-04-03 14:09:54 +0400

    first commit

---
+++ src/Components/authorSine/Sine.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./sine.scss";
+const Sine = () => {
+  return (
+    <div className="sine">
+      <p>
+        {" "}
+        <span>@WebSite Made By</span> "Giga||Shavliashvili"{" "}
+      </p>
+    </div>
+  );
+};
+
+export default Sine;
---
+++ src/Page/Tv.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tv = () => {
+  return <div>Tv serialss</div>;
+};
+
+export default Tv;
